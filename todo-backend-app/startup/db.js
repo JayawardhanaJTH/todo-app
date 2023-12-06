27aace98a62ac090c9db23b5b3d317314fe20241
JayawardhanaJTH <tiranharsha2323@gmail.com>
2023-12-06 22:04:07 +0530

    added dotenv

--- todo-backend-app/startup/db.js
+++ todo-backend-app/startup/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+console.log(process.env)
 
 module.exports = function(){
-  const db = "mmongodb+srv://tiranHarsha:tiran@123@cluster0.rwpfvjo.mongodb.net/?retryWrites=true&w=majority"
+  const db = `mmongodb+srv://${process.env.REACT_APP_MONGODB_USERNAME}:${process.env.REACT_APP_MONGODB_PASSWORD}@cluster0.rwpfvjo.mongodb.net/?retryWrites=true&w=majority`
     mongoose.connect(db,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
